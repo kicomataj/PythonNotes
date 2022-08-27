32ca73614919b7f2e91b94f4d17f7afd2b93747d
Adam Scanga <sandmanscanga@gmail.com>
2022-08-26 21:22:52 -0400

    Added documentation for lists, tuples, sets, and dictionaries

--- data_structures.py
+++ data_structures.py
@@ -2,7 +2,7 @@
 
 
 """
-Data Structures
+Primitive Data Structures
 
 list
     - [1, 2, 3]  SQUARE BRACKETS
@@ -12,16 +12,46 @@ list
     - mutable
 
 tuple
+    - (1, 2, 3)  PARENTHESIS
+    - iterable
+    - indexable
+    - ordered
+    - immutable
+
 set
+    - {1, 2, 3}  CURLY BRACKETS
+    - iterable
+    - NOT INDEXABLE
+    - NOT ORDERED
+    - immutable
+    - UNIQUE = CANNOT CONTAIN DUPLICATES
+
 dict
+    - {"key": "value"}  CURLY BRACKETS
+    - iterable
+    - NOT INDEXABLE (but you can perform lookups)
+    - ordered
+    - mutable
 
 """
+
+
+def learn_lists():
+    """Function to showcase the functionality of a list"""
+
+    # counter = 0
+    nums = [1, 2, 3, 4, 5]
+    for num in nums:
+        # counter += 1  # this gets called 3 times
+        # print(counter)
+        # print(num * num)  # finds square
+        print(num ** 3)
 
 
 def main():
     """Runs the main process"""
 
-    print("Something")
+    learn_lists()
 
 
 if __name__ == "__main__":
